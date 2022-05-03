5ad5611a7a2f859c4262fcb2174d68b2b17db3cb
Gabriel Nicutari <gabriel_nicutari@yahoo.ro>
2022-05-03 13:56:24 +0200

    Updated .env_example

--- src/index.js
+++ src/index.js
@@ -13,30 +13,30 @@ app.use(authRoutes);
 
 const user = process.env.MONGO_USERNAME;
 const password = process.env.MONGO_PASSWORD;
+const database = process.env.MONGO_DATABASE;
 
-const mongoUri =
-	`mongodb+srv://${user}:${password}@cluster0.ejki7.mongodb.net/jobPortal?retryWrites=true&w=majority`;
+const mongoUri = `mongodb+srv://${user}:${password}@cluster0.ejki7.mongodb.net/${database}?retryWrites=true&w=majority`;
 
 mongoose.connect(mongoUri, {
-	useNewUrlParser: true,
-	//useCreateIndex: true,
-	useUnifiedTopology: true
+  useNewUrlParser: true,
+  //useCreateIndex: true,
+  useUnifiedTopology: true
 });
 
 mongoose.connection.on('connected', () => {
-	console.log('Connected to mongo instance');
+  console.log('Connected to mongo instance');
 });
 
 app.get('/', requireAuth, (req, res) => {
-	console.log(req.user);
-	res.send(`Your email: ${req.user.email}`);
+  console.log(req.user);
+  res.send(`Your email: ${req.user.email}`);
 });
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, (error) => {
-	if (error) {
-		console.log(error);
-	}
-	console.log('Server is running on port', Number(PORT));
+  if (error) {
+    console.log(error);
+  }
+  console.log('Server is running on port', Number(PORT));
 });
