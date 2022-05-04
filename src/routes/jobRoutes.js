695800d9c174a6eada42f01d0d4ac81bd7d37a8f
Gabriel Nicutari <gabriel_nicutari@yahoo.ro>
2022-05-05 01:25:45 +0200

    jobRoutes res.status() corrections

--- src/routes/jobRoutes.js
+++ src/routes/jobRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const mySqlDb = require('../models/mysql/dbAssociations');
+const db = require('../models/mysql/dbAssociations');
 const { QueryTypes } = require('sequelize');
 
 router.post('/job', async (req, res) => {
   const date = new Date();
   const response = req.body.job;
-  console.log(req.body)
+
   const query1 = `call job_portal.add_job(
     "${response.title}", 
     "${response.company_id}", 
@@ -19,25 +19,35 @@ router.post('/job', async (req, res) => {
 
   // Initially, the job has to be created so that the new id is retrieved and used for the jobs-have-benefits table
   try {
-    const jobId = await mySqlDb.query(query1, { type: QueryTypes.INSERT });
+    const jobId = await db.query(query1, { type: QueryTypes.RAW });
 
     const benefits = req.body.benefits;
-    for (const benefit_id of benefits) {
-      const query2 = `
+    if (jobId) {
+      let ok = true;
+
+      for (const benefit_id of benefits) {
+        const query2 = `
         INSERT INTO \`jobs-have-benefits\` (job_id, benefit_id) 
         VALUES (${jobId[0].job_id}, ${benefit_id});
       `;
 
-      try {
-        await mySqlDb.query(query2, { type: QueryTypes.INSERT });
-      } catch {
-        res.status(500).send({ message: 'Could not create a new job!' });
+        try {
+          const result = await db.query(query2, { type: QueryTypes.INSERT });
+          if (!result) {
+            ok = false;
+          }
+        } catch {
+          res.status(500).send({ message: 'Could not create a new job!' });
+        }
+      }
+
+      if (ok) {
+        res.status(200).send({ messsage: 'The job has been added!' });
       }
     }
   } catch {
     res.status(500).send({ message: 'Could not create a new job!' });
   }
-  res.status(200).send({ message: 'The job has been added!' })
 });
 
 module.exports = router;
