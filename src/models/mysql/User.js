9fff25c7344e5838796fe6b163f7b4f164f61bc3
dim <dimitris.giokas1@gmail.com>
2022-05-04 08:11:07 +0200

    Revert "created ReadMe to show how to access model."
    This reverts commit 2bc5fddc

--- src/models/mysql/User.js
+++ src/models/mysql/User.js
@@ -15,6 +15,10 @@ module.exports = (sequelize) => {
                 allowNull: false,
                 unique: true,
             },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
             role_id: {
                 type: DataTypes.INTEGER,
                 allowNull: false,
