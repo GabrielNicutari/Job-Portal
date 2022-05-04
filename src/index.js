87a640a5c3b4a0e11ea6f10e96a237570d17a023
Gabriel Nicutari <gabriel_nicutari@yahoo.ro>
2022-05-04 19:44:56 +0200

    Init app self call

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const requireAuth = require('./middlewares/requireAuth');
 const db = require('./models/mysql/dbAssociations');
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(authRoutes);
 
 const user = process.env.MONGO_USERNAME;
@@ -17,35 +17,35 @@ const database = process.env.MONGO_DATABASE;
 const mongoUri = `mongodb+srv://${user}:${password}@cluster0.ejki7.mongodb.net/${database}?retryWrites=true&w=majority`;
 
 (initApp = async () => {
-    try {
-        mongoose.connect(mongoUri, {
-            useNewUrlParser: true,
-            //useCreateIndex: true,
-            useUnifiedTopology: true
-        });
+  try {
+    mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      //useCreateIndex: true,
+      useUnifiedTopology: true
+    });
 
-        mongoose.connection.on('connected', () => {
-            console.log('Connected to mongo instance');
-        });
+    mongoose.connection.on('connected', () => {
+      console.log('Connected to mongo instance');
+    });
 
-        app.get('/', requireAuth, (req, res) => {
-            console.log(req.user);
-            res.send(`Your email: ${req.user.email}`);
-        });
+    // app.get('/', requireAuth, (req, res) => {
+    //     console.log(req.user);
+    //     res.send(`Your email: ${req.user.email}`);
+    // });
 
-        const PORT = process.env.PORT || 8080;
+    const PORT = process.env.PORT || 8080;
 
-        console.log("Testing mysql connection..");
-        await db.authenticate();
-        console.log("Connection to mysql established successfully.");
+    console.log('Testing mysql connection..');
+    await db.authenticate();
+    console.log('Connection to mysql established successfully.');
 
-        app.listen(PORT, (error) => {
-            if (error) {
-                console.log(error);
-            }
-            console.log('Server is running on port', Number(PORT));
-        });
-    } catch (error) {
-        console.error("Unable to connect to the database:", error.original);
-    }
-});
+    app.listen(PORT, (error) => {
+      if (error) {
+        console.log(error);
+      }
+      console.log('Server is running on port', Number(PORT));
+    });
+  } catch (error) {
+    console.error('Unable to connect to the database:', error.original);
+  }
+})();
