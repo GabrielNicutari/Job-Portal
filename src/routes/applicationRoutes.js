cad2d52fb9355af6d7249fe4735ce91a274343f9
dim <dimitris.giokas1@gmail.com>
2022-05-05 03:19:40 +0200

    edited applicationRoutes.js

--- src/routes/applicationRoutes.js
+++ src/routes/applicationRoutes.js
@@ -4,11 +4,10 @@ const db = require('../models/mysql/dbAssociations');
 
 router.post('/application', async (req, res) => {
   try {
-    const newApplication = db.models.applications.build(req.body);
-    const result = await newApplication.save();
+    const newApplication = await db.models.applications.create(req.body);
 
-    if (result.dataValues) {
-      res.status(200).send({ message: 'The application has been added!' });
+    if (newApplication) {
+      res.status(200).send({ message: 'The application has been added!', newApplication });
     }
   } catch {
     res.status(500).send({ message: 'Could not create a new application!' });
