bee9418089d90cb39612d0aa982c3a826405ce87
Gabriel Nicutari <gabriel_nicutari@yahoo.ro>
2022-06-13 20:03:57 +0200

    User routes for graph db

--- src/routes/neo4j/authRoutes.js
+++ src/routes/neo4j/authRoutes.js
@@ -1,23 +1,26 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const neo4jDriver = require('../../database/neo4jConfig');
 
 const router = express.Router();
 const JWT_SECRET = process.env.JWT_SECRET;
 
-// router.post('/signup', async (req, res) => {
-//   const { email, password, username, role } = req.body;
-//
-//   try {
-//     const user = new User({ email, password, username, role });
-//     await user.save();
-//
-//     const token = jwt.sign({ userId: user._id }, JWT_SECRET);
-//     res.send({ token });
-//   } catch (e) {
-//     return res.status(422).send(e.message); // invalid data
-//   }
-// });
+const { register, login, comparePassword } = require('../../models/neo4j/User');
+
+router.post('/signup', async (req, res) => {
+  const { email, password, username, role } = req.body;
+
+  try {
+    const response = await register({ email, password, username, role });
+    const user = response.records[0]._fields[0];
+
+    console.log(user);
+
+    const token = jwt.sign({ userId: user.identity.low }, JWT_SECRET);
+    res.send({ token });
+  } catch (e) {
+    return res.status(422).send(e.message); // invalid data
+  }
+});
 
 router.post('/signin', async (req, res) => {
   const { email, password } = req.body;
@@ -25,27 +28,17 @@ router.post('/signin', async (req, res) => {
     return res.status(422).send({ error: 'Must provide email and password' });
   }
 
-  const session = neo4jDriver.session({database: 'neo4j'});
-
-  let user;
   try {
-    const result = await session.readTransaction(tx =>
-        tx.run(`MATCH (user:User) WHERE user.email = '${email}' return user`)
-    )
-    user = result.records[0]._fields[0].properties;
-    user.userId = result.records[0]._fields[0].identity.low;
-  } finally {
-    await session.close();
-  }
+    const response = await login({ email, password });
+    const user = response.records[0]._fields[0];
+    console.log(user);
 
-  if (!user) {
-    return res.status(404).send({ error: 'Invalid password or email' });
-  }
+    if (!user) {
+      return res.status(404).send({ error: 'Invalid password or email' });
+    }
 
-  try {
-    //TODO: fix compare password...
-    // await user.comparePassword(password);
-    const token = jwt.sign({ userId: user.userId }, JWT_SECRET);
+    await comparePassword(password, user.properties.password);
+    const token = jwt.sign({ userId: user.identity.low }, JWT_SECRET);
     res.send({ token });
   } catch (e) {
     return res.status(401).send({ error: 'Invalid password or email' });
