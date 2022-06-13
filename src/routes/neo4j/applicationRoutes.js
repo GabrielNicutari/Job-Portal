50f0ae848141a3701af03f5daf863a79714722a1
Gabriel Nicutari <gabriel_nicutari@yahoo.ro>
2022-06-13 16:06:09 +0200

    Another small fix

--- src/routes/neo4j/applicationRoutes.js
+++ src/routes/neo4j/applicationRoutes.js
@@ -4,19 +4,19 @@ const applicationModel = require('../../models/neo4j/Application');
 const { getPagination } = require('../helperFunctions');
 
 router.get('/applications', async (req, res) => {
-    let {page, size, email} = req.query;
-    if (!email) {
-        email = '';
-    }
-    const {limit, offset} = getPagination(page, size);
-    const result = await applicationModel.findAll(email, limit, offset);
-    res.json(result);
+  let { page, size, email } = req.query;
+  if (!email) {
+    email = '';
+  }
+  const { limit, offset } = getPagination(page, size);
+  const result = await applicationModel.findAll(email, limit, offset);
+  res.json(result);
 });
 
-router.post('/applications', async (req, res) => {
-    let newApplication = {...req.body, createdAt: new Date()};
-    const result = await applicationModel.create(newApplication);
-    res.json(result);
+router.post('/application', async (req, res) => {
+  let newApplication = { ...req.body, createdAt: new Date() };
+  const result = await applicationModel.create(newApplication);
+  res.json(result);
 });
 
 module.exports = router;
