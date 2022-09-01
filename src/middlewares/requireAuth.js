b96c2e9306ec2515bb9fc7cf5e5e9fce09963d8b
dim <dimitris.giokas1@gmail.com>
2022-09-01 13:52:37 +0200

    init

--- src/middlewares/requireAuth.js
+++ src/middlewares/requireAuth.js
@@ -1,6 +1,4 @@
 const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
-const User = mongoose.model('User');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
